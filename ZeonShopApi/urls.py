4653ad2139ac9bbfa413e9098d12c0b1a0564eec
uuljan <uuljan.alibaeva1@gmail.com>
2022-06-05 22:22:45 +0600

    Add AboutUs

--- ZeonShopApi/urls.py
+++ ZeonShopApi/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('main.urls')),
     path('api/v1/', include('product.urls')),
+    path('api/v1/', include('aboutUs.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path('api/v1/', include('callback_form.urls')),
---
+++ aboutUs/models.py
@@ -0,0 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class About(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    def save(self, *args, **kwargs):
+        if About.objects.exists() and not self.pk:
+            raise ValidationError('Экземпляр уже создан')
+        return super(About, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'О нас'
+        verbose_name_plural = 'О нас'
+
+    def __str__(self):
+        return self.title
+
+class Image_about(models.Model):
+    obj = models.ForeignKey(About, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products')
